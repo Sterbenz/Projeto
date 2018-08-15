b2e8b80f5952c60ee5bc2c9f69f6e375798202e7
v001 <v001@benner.com.br>
2018-08-15 14:03:28 -0300

    Template 2

---
+++ ProjetoFinal/Controllers/ProdutosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoFinal.DAO;
+using ProjetoFinal.Models;
+
+namespace ProjetoFinal.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+
+
+            return View();
+        }
+    }
+}
